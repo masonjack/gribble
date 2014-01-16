4f3f7f65f392a096cb5e8692db01005aac88a1ec
Jack Mason <jack.mason@ephox.com>
2014-01-16 17:35:10 +1000

    ADDED: render content.php in either a homepage mode or all-other-scenarios mode

--- content.php
+++ content.php
@@ -7,25 +7,36 @@
  */
 ?>
 
-<?php
-
-// HEEEEEEEEYYYYYY! Use the ishomepage methods to work out how to display this goodness you dingus!
-
-?>
-
-
-
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<?php if ( is_home() ) : ?>
 
                 <?php if ( has_post_thumbnail() ) : ?>
                 <div class="entry-thumbnail">
                         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'expound-featured' ); ?></a>
                 </div>
                 <?php endif; ?>
+<?php endif; ?>
 
                 <header class="entry-header">
                         <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'expound' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
                 </header><!-- .entry-header -->
+
+
+<?php if ( is_home() ) :
+
+                     // prints contents of custom fields
+                     $description = get_post_meta(get_the_ID(), "description", true);
+                     if ($description !== "") :
+                        echo "DESCRIPTION: $description";
+                     endif;
+
+                     $quote = get_post_meta(get_the_ID(), "quote", true);
+                     if ($quote !== "") :
+                        echo "QUOTE: $quote";
+                     endif;
+
+       else : ?>
 
                 <div class="entry-summary">
                         <?php the_excerpt(); ?>
@@ -33,6 +44,8 @@
                 </div><!-- .entry-summary -->
 
                 <div class="clear"></div>
+
+<?php endif; ?>
 
         </article>
 
--- index.php
+++ index.php
@@ -17,18 +17,6 @@
             if ( have_posts() ) :
                while ( have_posts() ) : the_post();
                      get_template_part( 'content', get_post_format() );
-                     
-                     // prints contents of custom fields
-                     $description = get_post_meta(get_the_ID(), "description", true);
-                     if ($description !== "") :
-                        echo "DESCRIPTION: $description";
-                     endif;
-
-                     $quote = get_post_meta(get_the_ID(), "quote", true);
-                     if ($quote !== "") :
-                        echo "QUOTE: $quote";
-                     endif;
-
                endwhile;
              endif;
          ?>
