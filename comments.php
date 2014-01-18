cee7096bee5e90e810ed483e1c1540731d465add
Jack Mason <jack.mason@ephox.com>
2014-01-18 17:16:55 +1000

    ADDED: Added links to Gravatar and changed 'reply yo' text for comment replies.

--- comments.php
+++ comments.php
@@ -8,12 +8,13 @@
 ?>
         <?php
            $comment_before_text = '<p class="comment-notes">' . __( 'Your email address will not be displayed. I will however add you to a spamming list for penis enlargement products (it\'s for your own good).' ) . ( $req ? $required_text : '' ) . '</p>';
-           $comment_after_text = '<p class="form-allowed-tags">' . sprintf( __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ),     ' <code>' . allowed_tags() . '</code>' ) . ' Keep in mind that CAPS LOCK works just as well, if not better.</p>';
+           $comment_after_text = '<p class="form-allowed-tags">You may use CAPS LOCK, but be gentle on your keyboard.</p><p class="form-allowed-tags">Click <a href="http://en.gravatar.com/" target="_blank">here</a> to give yourself a customizable avatar. Or don\'t, I don\'t give a fuck either way. Whatever makes you happy chappy.</p>';
  
            $comment_form_args = array(
               "title_reply" => "Impressed? Depressed? Tell me all about it.",
+              "title_reply_to" => __( ' Reply to and/or bitch-slap %s' ),
               "comment_notes_before" => $comment_before_text,
-              "comment_notes_after" => $comment_before_after
+              "comment_notes_after" => $comment_after_text
             );
            comment_form($comment_form_args); 
         ?>
