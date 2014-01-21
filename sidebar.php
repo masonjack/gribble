c49238dc2f032081dc5487603f384e59f9a961c6
Jack Mason <jack.mason@ephox.com>
2014-01-21 21:39:51 +1000

    ADDED: Comment text in sidebar reduced to an excerpt.

--- sidebar.php
+++ sidebar.php
@@ -31,10 +31,28 @@
 				$post = get_post($comment->comment_post_ID);
 				$post_permalink = get_post_permalink($comment->comment_post_ID);
 
+				$comment_auth_url = $comment->comment_author_url;
+				$commenter_display = "";
+				if ($comment_auth_url !== "") :
+					$commenter_display = '<a href="http://' . $comment_auth_url  . '">' . $comment->comment_author . '</a>';
+				else :
+					$commenter_display = $comment->comment_author;
+				endif;
+
+				$comment_content = $comment->comment_content;
+				$comment_content_excerpt = "";
+				$comment_content_excerpt_max_len = 60;
+
+                                if (strlen($comment_content) > $comment_content_excerpt_max_len) :
+                                        $comment_content_excerpt = substr($comment_content, 0, $comment_content_excerpt_max_len) . ' ...';
+                                else :
+                                        $comment_content_excerpt = $comment_content;
+                                endif;
+
 				echo('<br /><a href="' . $post_permalink . '#comment-' . $comment->comment_ID . '">' . $post->post_title . '</a>' .
-					'<br />Comment by ' . $comment->comment_author . 
+					'<br />Comment by ' . $commenter_display . 
 					' at ' . $comment->comment_date .
-					'<br />' . $comment->comment_content);
+					'<br />"' . $comment_content_excerpt . '"');
 			endforeach;	
 		?>
 	</aside>
