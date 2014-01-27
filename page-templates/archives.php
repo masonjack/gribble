087ff09db98992fca1eaf6f919f1ba02c75f4d0f
Jack Mason <jack.mason@ephox.com>
2014-01-27 14:40:52 +1000

    ADDED: Archives template renders content under month-year headers (likely bugs around here...)

--- page-templates/archives.php
+++ page-templates/archives.php
@@ -6,12 +6,52 @@ Description: Template for displaying all posts, grouping by published month.
 */
 
 get_header();
+?>
 
-// http://codex.wordpress.org/Function_Reference/get_posts
+      <div id=""primary>
 
+         <?php
+		$months = array(
+			"January", "February", "March", "April", "May", "June", "July",
+			"August", "September", "October", "November", "December");
+
+		$current_year = "";
+		$current_month = 0 ;
+
+		$posts = get_posts();
+		foreach ($posts as $post) :
+
+			$post_date = $post->post_date;
+			$space_index = strrpos($post_date, " ");
+			$post_date_small = substr($post_date, 0, $space_index);
+
+			$month = substr($post_date_small, 5, 2);
+			$month_int = (int) $month;
+
+			$year = substr($post_date, 0, 4);
+
+			if ($year !== $current_year || $current_month !== $month_int) :
+				$current_year = $year;
+				$current_month = $month_int;
+				echo "<h2>" . $months[$month_int - 1] . " " .  $year . "</h2>";
+			endif;
+
+			$link = '<article id="post-' . $post->ID . '">'; 
+                        $link .= '<a href="' . get_permalink( $post->ID ) . '">';
+                        $link .= $post->post_title;
+                        $link .= '</a>';
+			$link .= '</article>';
+
+			echo $link;
+		endforeach;
+
+         ?>
+
+      </div>
+
+<?php
 get_sidebar();
 get_footer();
-
 ?>
 
 
