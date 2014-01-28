9ddfbfb53c2c603982d3d8a2b7f1ad4edcb77c35
Jack Mason <jack.mason@ephox.com>
2014-01-28 22:03:38 +1000

    ADDED: Experiments with the banner background and formatting

--- header.php
+++ header.php
@@ -33,8 +33,11 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<header class="banner">
-				<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<div class="banner-text-holder">
+					<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</div>
 			</header>
 			<div id="main" class="site-main">
 
