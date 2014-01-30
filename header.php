4de3e46afcee7367f0603a6a039d1bc0fdbbc629
Jack Mason <jack.mason@ephox.com>
2014-01-30 22:22:06 +1000

    ADDED: Setting up page for responsive layout

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 		<title>
 		<?php 
 			if (is_home()) :
@@ -29,6 +29,7 @@
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
 		<?php wp_head(); ?>
+		<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/scripts/respond.min.js'></script>
 	</head>
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
